f707c31c32375b722d813e4cbca995ff43ceede1
MO <102580312+urkovera@users.noreply.github.com>
2026-04-10 17:05:51 +0000

    Setup piority queue feat: implement array-based priority queue for task scheduler

---
+++ queue.h
@@ -0,0 +1,14 @@
+typedef struct {
+    char name[50];
+    int deadline;
+} task;
+
+typedef struct {
+    task tasks[100];
+    int size;        
+} task_queue;
+
+
+void Initialize(task_queue *q);
+void Insert(task new_task, task_queue *q);
+task Extract(task_queue *q);
